75c1055a38734cacaa99ff18a4b89112135425b2
Abdullah06444 <abdullah44gur@gmail.com>
2020-08-23 02:47:40 +0300

    Added CSharp Code

---
+++ CSharp0002_EnterInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp0002_EnterInput
+{
+    class CSharp0002_EnterInput
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type something to the black board: ");
+            string inputt;
+            inputt = Console.ReadLine();
+            Console.WriteLine("Welcome to the CSharp programming language.");
+            Console.WriteLine("You were writing " + inputt + " to the black board. :D");
+        }
+    }
+}
+// C# string karakterlerimizde turkce kullanmaya izin veriyor.
